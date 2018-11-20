0051c227599c59c2a80b9722904a8c195fb0ee36
johanna.simonsson <johanna.simonsson@klarna.com>
2018-11-20 08:59:27 +0100

    Outline cli functions

--- yoink/cli.py
+++ yoink/cli.py
@@ -5,3 +5,19 @@ import click
 def cli():
     """Yoink CLI"""
     print('Save all your snippets')
+
+
+@click.command()
+def add():
+    """Add new snippet"""
+    pass
+
+@click.command()
+def add():
+    """Delete snippet"""
+    pass
+
+
+
+if __name__ == '__main__':
+    cli()
